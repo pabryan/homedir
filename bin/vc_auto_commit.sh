1a2e00d6ba77192281c6524e3a77862f7139616b
Paul Bryan <pabryan@gmail.com>
2012-10-25 11:01:44 -0700

    automatic commit via cron at math131.

--- bin/vc_auto_commit.sh
+++ bin/vc_auto_commit.sh
@@ -4,3 +4,6 @@ hg commit -m "automatic commit via cron at $HOSTNAME."
 
 cd ~/Dropbox/office/homedir
 hg commit -m "automatic commit via cron at $HOSTNAME."
+
+cd ~/Dropbox/office/working/ucsdwebsite
+hg commit -m "automatic commit via cron at $HOSTNAME."
