34899e0f71824c478bfc58139d4bb1dbbbce816d
Paul Bryan <pabryan@gmail.com>
2012-10-26 21:04:19 -0700

    automatic commit via cron at paul-Dell-System-Inspiron-N411Z.

--- bin/vc_auto_commit.sh
+++ bin/vc_auto_commit.sh
@@ -1,16 +1,8 @@
 #!/bin/bash
 
-dirs="~/org ~/Dropbox/${HOSTNAME}/homedir ~/working/ucsdwebsite"
-for dir in $dirs; do
-    echo $dir
+dirs="${HOME}/org ${HOME}/Dropbox/${HOSTNAME}/homedir ${HOME}/working/ucsdwebsite"
+for dir in ${dirs}; do
+    cd "${dir}"
+    hg commit -m "automatic commit via cron at $HOSTNAME."
 done
-
-cd ~/org
-hg commit -m "automatic commit via cron at $HOSTNAME."
-
-cd ~/Dropbox/$HOSTNAME/homedir
-hg commit -m "automatic commit via cron at $HOSTNAME."
-
-cd ~/working/ucsdwebsite
-hg commit -m "automatic commit via cron at $HOSTNAME."
 
