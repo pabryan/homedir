4c286d4535e98fdcd44d25d834980fe29f3f6709
Paul Bryan <pabryan@gmail.com>
2012-11-16 09:20:01 -0800

    automatic commit via cron at math131.

--- .emacs.d/site-start.d/org-mode/99-org-mode-init.el
+++ .emacs.d/site-start.d/org-mode/99-org-mode-init.el
@@ -3,7 +3,7 @@
 (setq org-agenda-files (quote ("~/org")))
 
 ;;; Modules
-;(setq org-modules (quote (org-habit)))
+(setq org-modules (quote (org-habit)))
 
 ;;; Key bindings
 
@@ -28,8 +28,8 @@
 (global-set-key (kbd "<f9> w") 'widen)
 ;(global-set-key (kbd "<f9> u") 'bh/narrow-up-one-level)
 
-;(global-set-key (kbd "<f9> I") 'bh/punch-in)
-;(global-set-key (kbd "<f9> O") 'bh/punch-out)
+(global-set-key (kbd "<f9> I") 'bh/punch-in)
+(global-set-key (kbd "<f9> O") 'bh/punch-out)
 
 ;(global-set-key (kbd "<f9> o") 'bh/make-org-scratch)
 
@@ -426,48 +426,48 @@ When not restricted, skip project and sub-project tasks, habits, and project rel
 (setq org-refile-target-verify-function 'bh/verify-refile-target)
 
 
-;;; Clock
+;; Clock
 ;;
 ;; Resume clocking task when emacs is restarted
-;; (org-clock-persistence-insinuate)
+(org-clock-persistence-insinuate)
 ;; ;;
-;; ;; Show lot sof clocking history so it's easy to pick items off the C-F11 list
-;; (setq org-clock-history-length 36)
-;; ;; Resume clocking task on clock-in if the clock is open
-;; (setq org-clock-in-resume t)
-;; ;; Change tasks to NEXT when clocking in
-;; (setq org-clock-in-switch-to-state 'bh/clock-in-to-next)
-;; ;; Separate drawers for clocking and logs
-;; (setq org-drawers (quote ("PROPERTIES" "LOGBOOK")))
-;; ;; Save clock data and state changes and notes in the LOGBOOK drawer
-;; (setq org-clock-into-drawer t)
-;; ;; Sometimes I change tasks I'm clocking quickly - this removes clocked tasks with 0:00 duration
-;; (setq org-clock-out-remove-zero-time-clocks t)
-;; ;; Clock out when moving task to a done state
-;; (setq org-clock-out-when-done t)
-;; ;; Save the running clock and all clock history when exiting Emacs, load it on startup
-;; (setq org-clock-persist t)
-;; ;; Do not prompt to resume an active clock
-;; (setq org-clock-persist-query-resume nil)
-;; ;; Enable auto clock resolution for finding open clocks
-;; (setq org-clock-auto-clock-resolution (quote when-no-clock-is-running))
-;; ;; Include current clocking task in clock reports
-;; (setq org-clock-report-include-clocking-task t)
+;; Show lot sof clocking history so it's easy to pick items off the C-F11 list
+(setq org-clock-history-length 36)
+;; Resume clocking task on clock-in if the clock is open
+(setq org-clock-in-resume t)
+;; Change tasks to NEXT when clocking in
+(setq org-clock-in-switch-to-state 'bh/clock-in-to-next)
+;; Separate drawers for clocking and logs
+(setq org-drawers (quote ("PROPERTIES" "LOGBOOK")))
+;; Save clock data and state changes and notes in the LOGBOOK drawer
+(setq org-clock-into-drawer t)
+;; Removes clocked tasks with 0:00 duration
+(setq org-clock-out-remove-zero-time-clocks t)
+;; Clock out when moving task to a done state
+(setq org-clock-out-when-done t)
+;; Save the running clock and all clock history when exiting Emacs, load it on startup
+(setq org-clock-persist t)
+;; Do not prompt to resume an active clock
+(setq org-clock-persist-query-resume nil)
+;; Enable auto clock resolution for finding open clocks
+(setq org-clock-auto-clock-resolution (quote when-no-clock-is-running))
+;; Include current clocking task in clock reports
+(setq org-clock-report-include-clocking-task t)
 
-;; (setq bh/keep-clock-running nil)
+(setq bh/keep-clock-running nil)
 
-;; (defun bh/clock-in-to-next (kw)
-;;   "Switch a task from TODO to NEXT when clocking in.
-;; Skips capture tasks, projects, and subprojects.
-;; Switch projects and subprojects from NEXT back to TODO"
-;;   (when (not (and (boundp 'org-capture-mode) org-capture-mode))
-;;     (cond
-;;      ((and (member (org-get-todo-state) (list "TODO"))
-;;            (bh/is-task-p))
-;;       "NEXT")
-;;      ((and (member (org-get-todo-state) (list "NEXT"))
-;;            (bh/is-project-p))
-;;       "TODO"))))
+(defun bh/clock-in-to-next (kw)
+  "Switch a task from TODO to NEXT when clocking in.
+Skips capture tasks, projects, and subprojects.
+Switch projects and subprojects from NEXT back to TODO"
+  (when (not (and (boundp 'org-capture-mode) org-capture-mode))
+    (cond
+     ((and (member (org-get-todo-state) (list "TODO"))
+           (bh/is-task-p))
+      "NEXT")
+     ((and (member (org-get-todo-state) (list "NEXT"))
+           (bh/is-project-p))
+      "TODO"))))
 
 (defun bh/find-project-task ()
    "Move point to the parent (project) task if any"
@@ -480,73 +480,73 @@ When not restricted, skip project and sub-project tasks, habits, and project rel
        (goto-char parent-task)
        parent-task)))
 
-;; (defun bh/punch-in (arg)
-;;   "Start continuous clocking and set the default task to the
-;; selected task.  If no task is selected set the Organization task
-;; as the default task."
-;;   (interactive "p")
-;;   (setq bh/keep-clock-running t)
-;;   (if (equal major-mode 'org-agenda-mode)
-;;       ;;
-;;       ;; We're in the agenda
-;;       ;;
-;;       (let* ((marker (org-get-at-bol 'org-hd-marker))
-;;              (tags (org-with-point-at marker (org-get-tags-at))))
-;;         (if (and (eq arg 4) tags)
-;;             (org-agenda-clock-in '(16))
-;;           (bh/clock-in-organization-task-as-default)))
-;;     ;;
-;;     ;; We are not in the agenda
-;;     ;;
-;;     (save-restriction
-;;       (widen)
-;;       ; Find the tags on the current task
-;;       (if (and (equal major-mode 'org-mode) (not (org-before-first-heading-p)) (eq arg 4))
-;;           (org-clock-in '(16))
-;;         (bh/clock-in-organization-task-as-default)))))
+(defun bh/punch-in (arg)
+  "Start continuous clocking and set the default task to the
+selected task.  If no task is selected set the Organization task
+as the default task."
+  (interactive "p")
+  (setq bh/keep-clock-running t)
+  (if (equal major-mode 'org-agenda-mode)
+      ;;
+      ;; We're in the agenda
+      ;;
+      (let* ((marker (org-get-at-bol 'org-hd-marker))
+             (tags (org-with-point-at marker (org-get-tags-at))))
+        (if (and (eq arg 4) tags)
+            (org-agenda-clock-in '(16))
+          (bh/clock-in-organization-task-as-default)))
+    ;;
+    ;; We are not in the agenda
+    ;;
+    (save-restriction
+      (widen)
+      ; Find the tags on the current task
+      (if (and (equal major-mode 'org-mode) (not (org-before-first-heading-p)) (eq arg 4))
+          (org-clock-in '(16))
+        (bh/clock-in-organization-task-as-default)))))
 
-;; (defun bh/punch-out ()
-;;   (interactive)
-;;   (setq bh/keep-clock-running nil)
-;;   (when (org-clock-is-active)
-;;     (org-clock-out))
-;;   (org-agenda-remove-restriction-lock))
+(defun bh/punch-out ()
+  (interactive)
+  (setq bh/keep-clock-running nil)
+  (when (org-clock-is-active)
+    (org-clock-out))
+  (org-agenda-remove-restriction-lock))
 
-;; (defun bh/clock-in-default-task ()
-;;   (save-excursion
-;;     (org-with-point-at org-clock-default-task
-;;       (org-clock-in))))
+(defun bh/clock-in-default-task ()
+  (save-excursion
+    (org-with-point-at org-clock-default-task
+      (org-clock-in))))
 
-;; (defun bh/clock-in-parent-task ()
-;;   "Move point to the parent (project) task if any and clock in"
-;;   (let ((parent-task))
-;;     (save-excursion
-;;       (save-restriction
-;;         (widen)
-;;         (while (and (not parent-task) (org-up-heading-safe))
-;;           (when (member (nth 2 (org-heading-components)) org-todo-keywords-1)
-;;             (setq parent-task (point))))
-;;         (if parent-task
-;;             (org-with-point-at parent-task
-;;               (org-clock-in))
-;;           (when bh/keep-clock-running
-;;             (bh/clock-in-default-task)))))))
+(defun bh/clock-in-parent-task ()
+  "Move point to the parent (project) task if any and clock in"
+  (let ((parent-task))
+    (save-excursion
+      (save-restriction
+        (widen)
+        (while (and (not parent-task) (org-up-heading-safe))
+          (when (member (nth 2 (org-heading-components)) org-todo-keywords-1)
+            (setq parent-task (point))))
+        (if parent-task
+            (org-with-point-at parent-task
+              (org-clock-in))
+          (when bh/keep-clock-running
+            (bh/clock-in-default-task)))))))
 
-;; (defvar bh/organization-task-id "eb155a82-92b2-4f25-a3c6-0304591af2f9")
+(defvar bh/organization-task-id "0a16188e-7a1a-4413-a516-5127dcbee30c")
 
-;; (defun bh/clock-in-organization-task-as-default ()
-;;   (interactive)
-;;   (org-with-point-at (org-id-find bh/organization-task-id 'marker)
-;;     (org-clock-in '(16))))
+(defun bh/clock-in-organization-task-as-default ()
+  (interactive)
+  (org-with-point-at (org-id-find bh/organization-task-id 'marker)
+    (org-clock-in '(16))))
 
-;; (defun bh/clock-out-maybe ()
-;;   (when (and bh/keep-clock-running
-;;              (not org-clock-clocking-in)
-;;              (marker-buffer org-clock-default-task)
-;;              (not org-clock-resolving-clocks-due-to-idleness))
-;;     (bh/clock-in-parent-task)))
+(defun bh/clock-out-maybe ()
+  (when (and bh/keep-clock-running
+             (not org-clock-clocking-in)
+             (marker-buffer org-clock-default-task)
+             (not org-clock-resolving-clocks-due-to-idleness))
+    (bh/clock-in-parent-task)))
 
-;; (add-hook 'org-clock-out-hook 'bh/clock-out-maybe 'append)
+(add-hook 'org-clock-out-hook 'bh/clock-out-maybe 'append)
 
 ;;; RefTeX
 (defun org-mode-reftex-setup ()
@@ -590,3 +590,49 @@ When not restricted, skip project and sub-project tasks, habits, and project rel
   '(progn
      ;; Change .pdf association directly within the alist
      (setcdr (assoc "\\.pdf\\'" org-file-apps) "emacs %s")))
+
+;; mobile org
+(setq org-mobile-directory "~/Dropbox/mobileorg")
+
+; push automatically
+(defvar org-mobile-push-timer nil
+  "Timer that `org-mobile-push-timer' used to reschedule itself, or nil.")
+
+(defun org-mobile-push-with-delay (secs)
+  (when org-mobile-push-timer
+    (cancel-timer org-mobile-push-timer))
+  (setq org-mobile-push-timer
+        (run-with-idle-timer
+         (* 1 secs) nil 'org-mobile-push)))
+
+(add-hook 'after-save-hook 
+ (lambda () 
+   (when (eq major-mode 'org-mode)
+     (dolist (file (org-mobile-files-alist))
+       (if (string= (expand-file-name (car file)) (buffer-file-name))
+           (org-mobile-push-with-delay 30)))
+   )))
+
+;; refreshes agenda file each day
+(run-at-time "00:05" 86400 '(lambda () (org-mobile-push-with-delay 1))) 
+
+;; pull automatically
+(org-mobile-pull) ;; run org-mobile-pull at startup
+
+(defun install-monitor (file secs)
+  (run-with-timer
+   0 secs
+   (lambda (f p)
+     (unless (< p (second (time-since (elt (file-attributes f) 5))))
+       (org-mobile-pull)))
+   file secs))
+
+(install-monitor (file-truename
+                  (concat
+                   (file-name-as-directory org-mobile-directory)
+                          org-mobile-capture-file))
+                 5)
+
+;; Do a pull every 5 minutes to circumvent problems with timestamping
+;; (ie. dropbox bugs)
+(run-with-timer 0 (* 5 60) 'org-mobile-pull)
