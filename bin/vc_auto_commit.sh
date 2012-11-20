137ee936db0a8916fdffb85b25aa4ed179164a50
Paul Bryan <pabryan@gmail.com>
2012-11-20 09:45:58 -0800

    Automated commit

--- bin/vc_auto_commit.sh
+++ bin/vc_auto_commit.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 dirs="${HOME}/org ${HOME}/Dropbox/${HOSTNAME}/homedir"
+
 for dir in ${dirs}; do
     cd "${dir}"
-    hg commit -m "automatic commit via cron at ${HOSTNAME}."
+    hg autosync -o
 done
 
