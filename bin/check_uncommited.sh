f781f66d16b4cf6aab9c6f5b01e4c15cb7368b99
Paul Bryan <pabryan@gmail.com>
2012-12-09 17:29:05 -0800

    Automated commit

---
+++ bin/check_uncommited.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+working="${HOME}/working"
+
+cd $working
+# Mercurial controlled projects
+projs=$(find . -iname '.hg')
+
+# Strip trailing .hg
+for proj in ${projs}; do
+  proj="${proj%.hg}"
+  cd $proj
+#  echo "*********************************************"
+#  echo "Checking ${proj}"
+#  echo "---------------------------------------------"
+  
+
+  hg summary | grep -q 'commit: (clean)'
+  if [ $? -ne 0 ]; then
+      echo "${proj} has uncommitted changes"
+  fi
+
+  hg incoming >/dev/null
+  if [ $? -eq 0 ]; then
+      echo "${proj} has incoming changes"
+  fi
+
+  hg outgoing >/dev/null
+  if [ $? -eq 0 ]; then
+      echo "${proj} has outgoing changes"
+  fi
+#  echo "---------------------------------------------"
+  cd $working
+done
