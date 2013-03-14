4c888b6dbbbfe0059b69e99c35a8b62285a1f4d5
Paul Bryan <pabryan@gmail.com>
2013-03-14 14:10:02 -0700

    Automated commit

--- .emacs.d/site-start.d/org-mode/99-org-mode-init.el
+++ .emacs.d/site-start.d/org-mode/99-org-mode-init.el
@@ -113,6 +113,10 @@
 	           (file+datetree "~/org/diary.org")
 		   "* %?\n%U\n" 
 		   :clock-in t :clock-resume t)
+	      ("R" "Research Note" entry 
+	           (file+datetree "~/org/research_notes.org")
+		   "* %?\n%U\n" 
+		   :clock-in t :clock-resume t)
               ("w" "org-protocol" entry 
 	           (file "~/org/refile.org")
 		   "* TODO Review %c\n%U\n" 
@@ -712,8 +716,8 @@ as the default task."
   (org-mobile-pull))
   
 ; push automatically
-(defvar org-mobile-push-timer nil
-  "Timer that `org-mobile-push-timer' used to reschedule itself, or nil.")
+;;(defvar org-mobile-push-timer nil
+;;  "Timer that `org-mobile-push-timer' used to reschedule itself, or nil.")
 
 (defun org-mobile-push-with-delay (secs)
   (when org-mobile-push-timer
@@ -722,13 +726,13 @@ as the default task."
         (run-with-idle-timer
          (* 1 secs) nil 'pb/org-mobile-push-wrapper)))
 
-(add-hook 'after-save-hook 
- (lambda () 
-   (when (eq major-mode 'org-mode)
-     (dolist (file (org-mobile-files-alist))
-       (if (string= (expand-file-name (car file)) (buffer-file-name))
-           (org-mobile-push-with-delay 30)))
-   )))
+;;(add-hook 'after-save-hook 
+;; (lambda () 
+;;   (when (eq major-mode 'org-mode)
+;;     (dolist (file (org-mobile-files-alist))
+;;       (if (string= (expand-file-name (car file)) (buffer-file-name))
+;;           (org-mobile-push-with-delay 30)))
+;;   )))
 
 ;; refreshes agenda file each day
 ;;(run-at-time "00:05" 86400 '(lambda () (org-mobile-push-with-delay 1))) 
@@ -744,15 +748,15 @@ as the default task."
        (pb/org-mobile-pull-wrapper)))
    file secs))
 
-(install-monitor (file-truename
-                  (concat
-                   (file-name-as-directory org-mobile-directory)
-                          org-mobile-capture-file))
-                 5)
+;;(install-monitor (file-truename
+;;                  (concat
+;;                   (file-name-as-directory org-mobile-directory)
+;;                          org-mobile-capture-file))
+;;                 5)
 
 ;; Do a pull every 5 minutes to circumvent problems with timestamping
 ;; (ie. dropbox bugs)
-(run-with-timer 0 (* 5 60) 'pb/org-mobile-pull-wrapper)
+;;(run-with-timer 0 (* 5 60) 'pb/org-mobile-pull-wrapper)
 
 ;; Latex settings
 ; default document structure
