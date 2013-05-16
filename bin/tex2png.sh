8a92d59c03403ebf15c61d82783e1bf7c05c0a12
Paul Bryan <pabryan@gmail.com>
2013-05-16 10:00:03 -0700

    Automated commit

---
+++ bin/tex2png.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+formula="${1}"
+out="${2}"
+
+head="\documentclass[border=2pt,varwidth]{standalone}
+\usepackage{standalone}
+\usepackage{amsmath}
+\usepackage[all]{pabmacros}
+\begin{document}"
+foot="\end{document}"
+echo "${head} ${formula} ${foot}" > "$HOME/tmp/formula.tex"
+pdflatex "\input{$HOME/tmp/formula.tex}"
+convert -density 150 formula.pdf -quality 90 $out.png
