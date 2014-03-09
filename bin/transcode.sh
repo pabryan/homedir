d6a92cce7f7682ee43f3cb8cfc87636ceb0c9f1a
Paul Bryan <pabryan@gmail.com>
2014-03-08 16:20:06 -0800

    Automated commit

--- bin/transcode.sh
+++ bin/transcode.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 input="$1"
+outdir="$2"
 format="mp4"
 
 #extension="${input##*.}"
-output="${input%.*}.${format}"
+output="${outdir}/${input%.*}.${format}"
 
 #Transcode
 ffmpeg -i "$input" -s 640x480 -vcodec mpeg4 -b 512k -acodec libvo_aacenc -ac 1 -ar 16000 -r 30 -ab 32000 -aspect 3:2 "$output"
