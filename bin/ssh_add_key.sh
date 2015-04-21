d87ec3962675da684307490452edc2ea602d7508
Paul Bryan <pabryan@gmail.com>
2015-04-21 14:13:32 -0700

    Automated commit

--- bin/ssh_add_key.sh
+++ bin/ssh_add_key.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
-ssh-add -l > /dev/null 2>&1
+# Make sure the agent is running
+if [ -z "$SSH_AUTH_SOCK" ] ; then
+  eval $(ssh-agent$)
+fi
 
+# Only add if we don't already have a key
+ssh-add -l > /dev/null 2>&1
 if [ $? -ne 0 ]; then
     ssh-add 
     if [ $? -ne 0 ]; then
