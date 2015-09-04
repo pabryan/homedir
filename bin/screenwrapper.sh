28e791d695854227f18f000135bb91a999d75853
Paul Bryan <pabryan@gmail.com>
2015-09-04 10:43:22 +1000

    Automated commit

--- bin/screenwrapper.sh
+++ bin/screenwrapper.sh
@@ -5,3 +5,7 @@ grabsession
 ssh_control_master.sh "hg@bitbucket.org"
 screen -xRR
 
+# list windows
+# screen -S $screen_name -Q windows
+# create emacs
+# screen -S $screen_name -X screen -t emacs emacsclient -nw -c
