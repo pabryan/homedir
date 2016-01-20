a18be266e27e81161276a9ac912b619deeaaff2c
Paul Bryan <pabryan@gmail.com>
2016-01-20 13:47:59 +0000

    Automated commit

--- .emacs.d/site-start.d/dired/99-dired-init.el
+++ .emacs.d/site-start.d/dired/99-dired-init.el
@@ -1,2 +1,12 @@
+;; Dired settings
+(require 'dired )
+
+; allow copying between windows
+(setq dired-dwim-target t)
+
+; Don't create new buffers
+(define-key dired-mode-map (kbd "<return>") 'dired-find-alternate-file) ; was dired-advertised-find-file
+(define-key dired-mode-map (kbd "^") (lambda () (interactive) (find-alternate-file "..")))  ; was dired-up-directory
+
 (add-hook 'dired-load-hook '(lambda () (require 'dired-x)))
 (setq dired-omit-mode t)
