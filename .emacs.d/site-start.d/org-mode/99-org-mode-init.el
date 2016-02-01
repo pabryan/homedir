ddb51f69f034418916fd457827d803509e1be65a
Paul Bryan <pabryan@gmail.com>
2016-02-01 11:16:39 +0000

    Automated commit

--- .emacs.d/site-start.d/org-mode/99-org-mode-init.el
+++ .emacs.d/site-start.d/org-mode/99-org-mode-init.el
@@ -1,3 +1,18 @@
+; Org-mode
+
+
+;;; For installing org-mode to latest package. I don't always want to do this so it's commented for the moment.
+;; (require 'package)
+;; (add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
+
+;; (package-initialize)
+;; (unless package-archive-contents    ;; Refresh the packages descriptions
+;;   (package-refresh-contents))
+;; (setq package-load-list '(all))     ;; List of packages to load
+;; (unless (package-installed-p 'org)  ;; Make sure the Org package is
+;;   (package-install 'org))           ;; installed, install it if not
+;; (package-initialize)                ;; Initialize & Install Package
+
 ;;; Directories
 (setq org-directory "~/org")
 (setq org-agenda-files (quote ("~/org")))
